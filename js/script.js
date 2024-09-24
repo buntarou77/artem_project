a104fa04c03765be56182175f25c83f81941cd8f
artem <artemtitaev0@gmail.ru>
2024-09-24 22:24:26 +0400

    2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-
-const numberOfFilms = parseFloat();
-console.log("введите калличество просмотренных вами фльимов", numberOfFilms);
+const numberOfFilms = +prompt('введите кал-во просмотренных вами фильмов:', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+     movies: {},
+     actors: {},
+     generes: [ ],
+     privat: false 
+};
+const a = prompt("один из последних просмотренных фильмов", ""),
+b = prompt("на сколько оцените его?", ""),
+c =  prompt("один из последних просмотренных фильмов", ""),
+d = prompt("на сколько оцените его?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
