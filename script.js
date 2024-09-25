f2e1ec67eb2f7cbfbe478abd3ee72b43e9b7713b
artem <artemtitaev0@gmail.ru>
2024-09-26 00:46:29 +0400

    yes

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const hambirger = 5;
+const fries = null;
+if(){
+    console.log("я сыт!");
+}*/
+const hamburger = 3;
+const cola = 3;
+const fries = 21;
+ console.log(hamburger === 3 && cola && fries);
+
+ console.log(0 && 21212121);
+ console.log( 1 && 5 );
+ console.log(null && 5);
+ console.log(1 && "wqwqwqwqwq");
+
+ if(hamburger === 3 && cola && fries){
+    console.log("1");
+ } else{
+    console.log("3");
+ }
