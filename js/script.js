09fb21e7b585d1bb8954733ede34206855cfc6a2
artem <artemtitaev0@gmail.ru>
2024-10-16 21:11:57 +0400

    learn

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-//console.log(document.head);
-//console.log(document.documentElement);
-//console.log(document.body.childNodes);
-//console.log(document.body.firstElementChild);
-//console.log(document.body.lastElementChild);
-//console.log(document.querySelector("#current").parentElement);
-console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
-//for(let node of document.body.childNodes){
-//if(node.nodeName == "#text"){
- //   continue
-//}
-//    console.log(node);
-//}
+function pow(x, n){
+    let result = 1;
+    for(let i = 0; i < n; i++){
+        result *= x;
+    } 
+    console.log(result);
+    return result;
+}
+function pow1(x, n){
+if(n === 1){ 
+    return x;debugger;
+}else{
+    return x * pow1(x, n - 1);debugger;
+}
+}
+pow1(2, 4);
+
+
+
