bd1ddf3f98fd05720dfac2fb91c6efa56c32f205
artem <artemtitaev0@gmail.ru>
2024-10-19 12:50:18 +0400

    work

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-function pow(x, n){
-    let result = 1;
-    for(let i = 0; i < n; i++){
-        result *= x;
-    } 
-    console.log(result);
-    return result;
-}
-function pow1(x, n){
-if(n === 1){ 
-    return x;debugger;
-}else{
-    return x * pow1(x, n - 1);debugger;
-}
-}
-pow1(2, 4);
-
-
-
+const str = "";
+console.log(typeof str)
